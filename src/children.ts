10ae73ff4967c85b9014314cfd755d25b8c32886
iamhabbeboy <iamhabbeboy@gmail.com>
2024-03-09 22:00:02 +0100

    Add address field

--- src/children.ts
+++ src/children.ts
@@ -6,13 +6,16 @@ export function setupChildren() {
   form.addEventListener("submit", async (evt) => {
     evt.preventDefault();
     const parentName = document.getElementById(
-      "parent-name"
+      "parent-name",
     ) as HTMLInputElement;
     const parentPhone = document.getElementById(
-      "parent-phone"
+      "parent-phone",
     ) as HTMLInputElement;
     const childName = document.getElementById("child-name") as HTMLInputElement;
     const childDob = document.getElementById("child-dob") as HTMLDataElement;
+    const homeAddress = document.getElementById(
+      "home-address",
+    ) as HTMLTextAreaElement;
     const img = document.getElementById("preview") as HTMLImageElement;
     if (img.src === "") {
       return alert("Kindly capture an image before you proceed");
@@ -20,29 +23,31 @@ export function setupChildren() {
     const start = new Date(childDob.value);
     const end = new Date();
     let years = end.getFullYear() - start.getFullYear();
-  let months = end.getMonth() - start.getMonth() + 1;
+    let months = end.getMonth() - start.getMonth() + 1;
 
-  // Adjust for months if the end month is less than the start month
-  if (months < 0) {
-    years--;
-    months += 12;
-  }
-  console.log("months: ", months, "years: ", years)
+    // Adjust for months if the end month is less than the start month
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
     const child: IChildren = {
-        age: years === 0 ? months : years,
-        ageData: {
-            months, years
-        },
-        name: childName.value,
-        dob: childDob.value,
-        parentName: parentName.value,
-        parentPhone: parentPhone.value,
-        image: img.src,
+      age: years === 0 ? months : years,
+      ageData: {
+        months,
+        years,
+      },
+      name: childName.value,
+      dob: childDob.value,
+      parentName: parentName.value,
+      parentPhone: parentPhone.value,
+      homeAddress: homeAddress.value,
+      image: img.src,
     };
     const res = await db.children.add(child as IChildren);
-    if(res) {
-        alert("Record added succesfully!");
-        window.location.href = "/children/"
+    if (res) {
+      alert("Record added succesfully!");
+      window.location.href = "/children/";
     }
     return alert("Error occured!");
   });
@@ -52,26 +57,27 @@ export function setupChildren() {
 async function getAllChildren() {
   const getAllChildren = await db.children.where("age").above(0).toArray();
   const childrenList = document.getElementById("children-list") as HTMLElement;
-    let result = '';
-    getAllChildren.forEach((child) => {
-        result +=  `<div class="children">
+  let result = "";
+  getAllChildren.forEach((child) => {
+    result += `<div class="children">
         <div class="children__layout">
           <img
             src="${child.image}"
-            style="border-radius: 100%; width: 150px; height: 150px"
+            style="border-radius: 9px;width: 150px; height: 150px"
           />
           <div class="children__name">
             <h3>${child.name}</h3>
-            <h3>Age: ${child.ageData.months} Months, ${child.ageData.years} Years</h3>
+            <h3>Age: ${child.ageData.years} Years, ${child.ageData.months} Months</h3>
             <p>Parent Name: ${child.parentName}</p>
             <p>Parent Phone No.: ${child.parentPhone}</p>
+            ${child.homeAddress ? "<p>Address: " + child.homeAddress + "</p>" : ""}
           </div>
-          <div class="children__option">
+          <!--<div class="children__option">
             <button>Check-in</button>
             <button>Check-in</button>
-          </div>
+          </div>-->
         </div>
-      </div>`
-    })
-    childrenList.innerHTML = result
+      </div>`;
+  });
+  childrenList.innerHTML = result;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,41 @@
 export interface IReport {
-    id: number;
-    date: number;
-    service: string;
-    offering: string;
-    main: Attendance;
-    overflow: Attendance;
-    extension: Attendance;
-    teens: ChildrenAttendance;
-    children: ChildrenAttendance;
-    other: OtherAttendance;
-  }
+  id: number;
+  date: number;
+  service: string;
+  offering: string;
+  main: Attendance;
+  overflow: Attendance;
+  extension: Attendance;
+  teens: ChildrenAttendance;
+  children: ChildrenAttendance;
+  other: OtherAttendance;
+}
 
-  export interface IChildren {
-    age: number;
-    ageData: ChildAge,
-    name: string;
-    dob: string;
-    parentName: string;
-    parentPhone: string;
-    image: string;
-  }
+export interface IChildren {
+  age: number;
+  ageData: ChildAge;
+  name: string;
+  dob: string;
+  parentName: string;
+  parentPhone: string;
+  homeAddress: string;
+  image: string;
+}
 
-  interface ChildAge {
-    months: number;
-    years: number;
-  }
+interface ChildAge {
+  months: number;
+  years: number;
+}
 
 export interface OtherAttendance {
-    [key:string]: number; // traffic and info desk
+  [key: string]: number; // traffic and info desk
 }
-  
+
 export interface Attendance {
-    adult: number;
-    baby: number;
-  }
-  
-export interface ChildrenAttendance extends Pick<Attendance, 'adult'>{
-    children: number;
-  }
+  adult: number;
+  baby: number;
+}
+
+export interface ChildrenAttendance extends Pick<Attendance, "adult"> {
+  children: number;
+}
